4541083a2b7ac12fbd002066e8ce27532b05e4e3
fada.yu <fada.yu@zhaopin.com.cn>
2020-08-21 10:59:57 +0800

    first commit

---
+++ src/main/java/com/evan/wj/SpringJotterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.evan.wj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJotterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJotterBackendApplication.class, args);
+    }
+
+}
